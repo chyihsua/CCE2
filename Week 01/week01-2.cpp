d098ab20c38440e7bf68fa74e98bfd62d481b9a7
陳映瑄 <91170636+chyihsua@users.noreply.github.com>
2022-02-24 17:41:54 +0800

    Add files via upload

---
+++ Week 01/week01-2.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    char a;
+    printf("請輸入一堆字元: ");
+    scanf("%c",&a);
+    printf("你讀到了 %c",a);
+}
---
+++ Week 01/week01-5.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+char line[2000];
+
+int main()
+{
+	int total=0;
+	while(scanf("%s",line)==1)
+	{
+		int N= strlen(line);
+		int ans=0;
+		for (int i=0;i<N;i++)
+		{
+			if (line[i]=='2') ans++;
+		}
+		printf("%d\n",ans);
+		total+=ans;
+	}
+	printf("Total: %d\n",total);
+		return 0;
+}
