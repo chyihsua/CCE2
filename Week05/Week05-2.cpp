6777a24ff9167fb807599bdad04bb209830547c9
陳映瑄 <91170636+chyihsua@users.noreply.github.com>
2022-03-24 17:16:20 +0800

    Add files via upload

---
+++ Week05/Week05-2.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int a[10]={4,5,6,1,2,3,7,8,9,0};
+
+int main()
+{
+    for(int k=0;k<10-1;k++)
+    {
+        for (int i=0;i<10-1;i++)
+        {
+            if (a[i]>a[i+1])
+                {
+                    int temp=a[i];
+                    a[i]=a[i+1];
+                    a[i+1]=temp;
+                }
+        }
+    }
+
+    for (int i=0;i<10;i++) printf("%d ",a[i]);
+    return 0;
+}
