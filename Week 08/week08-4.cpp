203bdcfb787fc9b6a7c5dadf1f5bade887eec653
chyihsua <ain0965416543@gmail.com>
2022-04-15 19:33:07 +0800

    add week.8

---
+++ Week 08/week08-4.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    FILE * fout=fopen("hello.txt","w+");
+    ///檔案的指標 要打開 w+是開啟方式write+
+    fprintf(fout,"Helllo 我在檔案裡,哈哈");
+
+    printf("Hello World\n");
+}
